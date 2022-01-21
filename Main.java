3dd61a784c5d3f80442fd5bb261053c5365aefdc
dlamasAeon96 <97554776+dlamasAeon96@users.noreply.github.com>
2022-01-21 15:37:27 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class CollectionMethods{
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+
+        names.add("David");
+        names.add("John");
+        names.add("Maria");
+        names.add("Carl");
+        names.add("Paul");
+        names.add("Andy");
+        names.add("Jayce");
+        names.add("Vanessa");
+        System.out.println("Names:");
+        for(String s : names){
+            System.out.println(s);
+        }
+        System.out.println("\nAfter sorting: ");
+        Collections.sort(names);
+        for(String s : names){
+            System.out.println(s);
+        }
+        System.out.println("\nAfter shuffle:");
+        Collections.shuffle(names);
+        for(String s : names){
+            System.out.println(s);
+        }
+        String findElement = "John";
+        String notFoundElement = "ABD";
+        System.out.println("Positon of "+findElement+": "+names.indexOf(findElement));
+        System.out.println("Positon of "+notFoundElement+": "+names.indexOf(notFoundElement));
+        System.out.println("\nAfter converting to array:");
+        String[] arr = new String[names.size()];
+        arr = names.toArray(arr);
+        for(String s : arr){
+            System.out.println(s);
+        }
+        System.out.println("\nAfter converting to list:");
+        List<String> list = Arrays.asList(arr);
+        for(String s : list){
+            System.out.println(s);
+        }
+    }
+}
